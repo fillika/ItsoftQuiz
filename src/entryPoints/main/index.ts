e2b5d8c2b51935c6d823d5915f3c879fe4600a0d
Aleksandr Mironenko <alfie.mir@gmail.com>
2021-02-14 13:47:46 +0300

    Правки структуры сайта. Переименовал папки

--- src/entryPoints/main/index.ts
+++
@@ -1,5 +0,0 @@
-import '../../scripts/main/js/index';
-import '../../scripts/main/ts/index';
-
-import '../../styles/main/css/index.css';
-import '../../styles/main/scss/index.scss';
---
+++ src/entryPoints/quiz/index.ts
@@ -0,0 +1,5 @@
+import '../../scripts/quiz/js/index';
+import '../../scripts/quiz/ts/index';
+
+import '../../styles/quiz/css/index.css';
+import '../../styles/quiz/scss/index.scss';
